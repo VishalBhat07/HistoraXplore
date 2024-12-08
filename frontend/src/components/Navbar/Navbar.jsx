df454a90183076c6210020d5b11d52368269f064
VishalBhat07 <vishalbhat21092005@gmail.com>
2024-12-08 22:11:45 +0530

    Changed navbar styles

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -4,18 +4,18 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <header>
-      <nav className="bg-orange-500 p-4 flex justify-between items-center border-black">
-        <div className="font-bold text-5xl text-gray-950">Histora Xplore</div>
+      <nav className="bg-black p-4 flex justify-between items-center border-black">
+        <div className="font-bold text-5xl text-white">Histora Xplore</div>
         <ul className="flex items-center space-x-4">
           <li>
-            <Link to="/" className="text-gray-950 text-xl font-semibold px-4">
+            <Link to="/" className="text-white text-xl font-semibold px-4">
               Home
             </Link>
           </li>
           <li>
             <Link
               to="/explore"
-              className="text-gray-950 text-xl font-semibold px-4"
+              className="text-white text-xl font-semibold px-4"
             >
               Explore
             </Link>
@@ -23,7 +23,7 @@ const Navbar = () => {
           <li>
             <Link
               to="/discuss"
-              className="text-gray-950 text-xl font-semibold px-4"
+              className="text-white text-xl font-semibold px-4"
             >
               Discuss
             </Link>
@@ -31,7 +31,7 @@ const Navbar = () => {
           <li>
             <Link
               to="/about"
-              className="text-gray-950 text-xl font-semibold px-4"
+              className="text-white text-xl font-semibold px-4"
             >
               About
             </Link>
@@ -39,7 +39,7 @@ const Navbar = () => {
           <li>
             <Link
               to="/contact"
-              className="text-gray-950 text-xl font-semibold px-4"
+              className="text-white text-xl font-semibold px-4"
             >
               Contact
             </Link>
