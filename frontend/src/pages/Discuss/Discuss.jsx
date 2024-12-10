59e3b5f98e8b37d4d06148b6f77a4bb1dc473515
SRNI-2005 <vssreenivaas2005@gmail.com>
2024-12-11 01:36:10 +0530

    Added posts to discussion forum, to do -> render comments in each post

--- frontend/src/pages/Discuss/Discuss.jsx
+++ frontend/src/pages/Discuss/Discuss.jsx
@@ -1,13 +1,31 @@
 import React, { useState, useEffect } from "react";
 import AddPost from "./AddPost";
 import PostCard from "./PostCard";
+import { getPosts } from "../../../firebaseFunctions/discussion";
 
-function Discuss(){
+function Discuss() {
+  const [posts, setPosts] = useState(null);
+
+  useEffect(() => {
+    async function fetchPosts() {
+      try {
+        const posted = await getPosts();
+        if (posted){
+          setPosts(posted);
+        }
+      } catch(error) {
+        console.error("Could not fetch posts", error);
+      }
+    }
+    fetchPosts();
+  }, [posts]);
+
   return (
     <>
-    <AddPost/>
+      <AddPost />
+      {posts == null ? null : posts.map((post)=><PostCard postId={post.id}/>)}
     </>
-  )
+  );
 }
 
 export default Discuss;
